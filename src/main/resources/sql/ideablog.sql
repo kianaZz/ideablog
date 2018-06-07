006242924a999c2bd93abb0f4c5909c461a43c07
kianaZz <1002418807@qq.com>
2018-06-07 17:27:50 +0800

    blog start!

--- src/main/resources/sql/ideablog.sql
+++ src/main/resources/sql/ideablog.sql
@@ -8,5 +8,10 @@ DROP TABLE IF EXISTS `logs`;
 
 CREATE TABLE `logs` (
   'id' int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `action` varchar(100) DEFAULT NULL ,
+  `data` varchar(2000) DEFAULT NULL ,
+  `author_id` int(10) DEFAULT NULL ,
+  `ip` varchar(20) DEFAULT NULL ,
+  `created` int(10) DEFAULT NULL ,
   PRIMARY KEY ('id')
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
